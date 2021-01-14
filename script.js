7ec647d756dba2b263574cc59b948c090a79b7b1
netsy001 <nettem.surendra@gmail.com>
2021-01-14 14:18:08 +1030

    Commiting changes to local storage

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 $(document).ready(function () {
-    
+
     function getTime() {
         var clock = moment().format('MMMM Do YYYY, h:mm:ss a');
         $("#currentDay").text(clock);
     }
     setInterval(getTime, 1000);
 
-$(".row").each(function(){
-    var currentTime = moment().hour();
-    console.log(currentTime)
-    var time =$(".time-block").attr("id");
-    console.log(time)
-    if (time < currentTime) {
-        console.log(time)
+    $(".row").each(function () {
+        var currentTime = moment().hour();
+        // console.log(currentTime)
+        var time = parseInt($(this).attr("data-hour"));
+        // console.log(time)
+        if (time < currentTime) {
+            // console.log(time)
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
@@ -29,4 +29,12 @@ $(".row").each(function(){
             $(this).addClass("future");
         }
     });
+
+    $(".saveBtn").click(function () {
+      var inputValue = $(".description").value;
+      localStorage.setItem(".description", inputValue);
+      $(".description").text(localStorage.getItem("inputValue"));
+    $(".saveBtn").click(empty);
+    });
 });
+
